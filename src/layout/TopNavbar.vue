fa5ada83b4cd0cf353c7ef3c4b0f59712233a64c
farisqlail <faris.riskilail@gmail.com>
2023-10-02 15:13:00 +0700

    first commit

---
+++ src/layout/TopNavbar.vue
@@ -0,0 +1,52 @@
+<template>
+  <nav class="navbar navbar-expand-lg">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Dashboard</a>
+      <div class="collapse navbar-collapse justify-content-end">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              Log out
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+<script>
+  export default {
+    computed: {
+      routeName () {
+        const {name} = this.$route
+        return this.capitalizeFirstLetter(name)
+      }
+    },
+    data () {
+      return {
+        activeNotifications: false
+      }
+    },
+    methods: {
+      capitalizeFirstLetter (string) {
+        return string.charAt(0).toUpperCase() + string.slice(1)
+      },
+      toggleNotificationDropDown () {
+        this.activeNotifications = !this.activeNotifications
+      },
+      closeDropDown () {
+        this.activeNotifications = false
+      },
+      toggleSidebar () {
+        this.$sidebar.displaySidebar(!this.$sidebar.showSidebar)
+      },
+      hideSidebar () {
+        this.$sidebar.displaySidebar(false)
+      }
+    }
+  }
+
+</script>
+<style>
+
+</style>
---
+++ src/pages/UserProfile.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="content">
+    <div class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-12">
+            <card class="strpied-tabled-with-hover" body-classes="table-full-width table-responsive">
+              <template slot="header">
+                <h4 class="card-title">Tabel Pengguna</h4>
+                <p class="card-category"></p>
+              </template>
+              <l-table class="table-hover table-striped" :columns="table1.columns" :data="table1.data">
+              </l-table>
+            </card>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+ import LTable from 'src/components/Table.vue'
+  import Card from 'src/components/Cards/Card.vue'
+  const tableColumns = ['No', 'Nama', 'Email', 'Status', 'Aksi']
+  const tableData = [{
+    no: 1,
+    nama: 'Dakota Rice',
+    email: '$36.738',
+    status: 'Niger',
+    aksi: 'Oud-Turnhout'
+  },
+  {
+    no: 2,
+    nama: 'Minerva Hooper',
+    email: '$23,789',
+    status: 'Curaçao',
+    aksi: 'Sinaai-Waas'
+  },
+  {
+    no: 3,
+    nama: 'Sage Rodriguez',
+    email: '$56,142',
+    status: 'Netherlands',
+    aksi: 'Baileux'
+  },
+  {
+    no: 4,
+    nama: 'Philip Chaney',
+    email: '$38,735',
+    status: 'Korea, South',
+    aksi: 'Overland Park'
+  },
+  {
+    no: 5,
+    nama: 'Doris Greene',
+    email: '$63,542',
+    status: 'Malawi',
+    aksi: 'Feldkirchen in Kärnten'
+  }]
+  export default {
+    components: {
+      LTable,
+      Card
+    },
+    data () {
+      return {
+        table1: {
+          columns: [...tableColumns],
+          data: [...tableData]
+        },
+        table2: {
+          columns: [...tableColumns],
+          data: [...tableData]
+        }
+      }
+    }
+  }
+
+</script>
+<style></style>
